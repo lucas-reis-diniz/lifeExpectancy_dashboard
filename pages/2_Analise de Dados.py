dfea7955a4388df0936eeb82f5e0c4f47309221a
Lucas Reis Diniz <lucasreis.dnz@gmail.com>
2025-03-27 13:23:40 -0300

    fix doenças

--- pages/2_Analise de Dados.py
+++ pages/2_Analise de Dados.py
@@ -75,27 +75,12 @@ st.plotly_chart(fig)
 
 options = st.multiselect(
     "Escolha as análises que deseja visualizar:",
-    ["Distribuição da Expectativa de Vida",
-     "Expectativa de Vida por País",
+    [
      "Doenças por Região",
      "Correlação entre PIB e Expectativa de Vida",
-     "Impacto da Vacinação na Expectativa de Vida"]
+     "Impacto da Vacinação na Expectativa de Vida"
+    ]
 )
-
-if "Distribuição da Expectativa de Vida" in options:
-    st.subheader("Distribuição da Expectativa de Vida")
-    fig = px.histogram(df, x="Life expectancy", nbins=30, title="Histograma da Expectativa de Vida",
-                       labels={"Life expectancy": "Expectativa de Vida"}, opacity=0.7, marginal="box")
-    st.plotly_chart(fig)
-
-if "Expectativa de Vida por País" in options:
-    st.subheader("Expectativa de Vida por País")
-    country = st.selectbox("Selecione um país:", df["Country"].unique())
-    df_country = df[df["Country"] == country]
-    fig = px.line(df_country, x="Year", y="Life expectancy", markers=True,
-                  title=f"Expectativa de Vida ao Longo dos Anos em {country}",
-                  labels={"Life expectancy": "Expectativa de Vida", "Year": "Ano"})
-    st.plotly_chart(fig)
 
 if "Doenças por Região" in options:
     # Mapa Coroplético para visualizar doenças por região
