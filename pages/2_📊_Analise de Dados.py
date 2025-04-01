eb10a8db0a8d6dcd367d2ed17572a90532800bb7
Lucas Reis Diniz <145406645+lucas-reis-diniz@users.noreply.github.com>
2025-04-01 19:05:51 -0300

    Update 2_📊_Analise de Dados.py

--- pages/2_📊_Analise de Dados.py
+++ pages/2_📊_Analise de Dados.py
@@ -121,3 +121,5 @@ st.plotly_chart(fig)
 
 st.write(f"**Média da Expectativa de Vida:** {mean_life:.2f} anos")
 st.write(f"**Intervalo de Confiança de 95%:** [{confidence_interval[0]:.2f}, {confidence_interval[1]:.2f}] anos")
+
+st.write("Caculo para o intervalo de confiança com 95% de certeza com base nos dados")
